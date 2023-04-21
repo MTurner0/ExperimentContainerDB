760c69d1fe411d676f5df09f444c8750fb75294b
MTurner0 <margaret.turner1416@gmail.com>
2023-04-21 16:08:22 -0500

    Simulate SEs.

---
+++ r/dummy_exp_builder.R
@@ -0,0 +1,47 @@
+# Makes a dummy SummarizedExperiment object of specified size.
+dummy_se_builder <- function(
+    data_generating_process,
+    params,
+    n_features = 10,
+    n_samples = 10,
+    seed
+    ) {
+
+  if (!missing(seed)) {
+    set.seed(seed)
+  }
+
+  params["n"] <- n_features * n_samples
+  data <- do.call(data_generating_process, params)
+  assay <- matrix(data, n_features, n_samples)
+  
+  coldat <- data.frame(sampleID = keygen(
+    n_samples, ceiling(log(n_samples, base = 62))
+    ))
+  colnames(assay) <- coldat$sampleID
+
+  rowdat <- data.frame(featureID = keygen(
+    n_features, ceiling(log(n_features, base = 62))
+    ))
+  rownames(assay) <- rowdat$featureID
+
+  SummarizedExperiment::SummarizedExperiment(
+    assays = list("dummy_assay" = assay),
+    rowData = rowdat,
+    colData = coldat
+  )
+}
+
+# Used to make unique keys for features and samples
+keygen <- function(n_keys, keylength = 10) {
+  keys <- character(length = 0L)
+  while(length(keys) < n_keys) {
+    key <- paste(
+      sample(c(letters, LETTERS, 0:9), size = keylength, replace = TRUE),
+      collapse = ""
+      )
+    keys <- c(keys, key)
+    keys <- unique(keys) # Remove duplicates
+  }
+  return(keys)
+}
