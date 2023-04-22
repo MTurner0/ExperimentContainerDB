a8c856932331a2c6bc607f5cf90c42791dc629bb
MTurner0 <margaret.turner1416@gmail.com>
2023-04-21 20:12:51 -0500

    Export SE objects to sqlite.

---
+++ r/exp_export.R
@@ -0,0 +1,31 @@
+library(DBI)
+library(dplyr)
+source("r/dummy_exp_builder.R")
+
+baby <- dummy_se_builder(rpois, list(lambda = 1), seed = 1416)
+
+baby.db <- dbConnect(RSQLite::SQLite(), "data/baby.sqlite")
+
+SummarizedExperiment::colData(baby) %>%
+  data.frame() %>%
+  dbWriteTable(baby.db, "colData", .)
+
+SummarizedExperiment::rowData(baby) %>%
+  data.frame() %>%
+  dbWriteTable(baby.db, "rowData", .)
+
+unpivot_assay <- function(assay) {
+  assay <- as.data.frame(assay)
+  assay$featureID <- rownames(assay)
+  
+  tidyr::pivot_longer(assay,
+                      !featureID,
+                      names_to = "sampleID",
+                      values_to = "value")
+}
+
+SummarizedExperiment::assay(baby) %>%
+  unpivot_assay() %>%
+  dbWriteTable(baby.db, "assay", .)
+
+dbListTables(baby.db)
