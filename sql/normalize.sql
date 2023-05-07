b7a22a96bac1634ac0ea572acd011b6bd8f444a8
MTurner0 <margaret.turner1416@gmail.com>
2023-05-07 10:27:00 -0500

    LN -> LOG

--- sql/normalize.sql
+++ sql/normalize.sql
@@ -1,6 +1,6 @@
 /* NOTE: Use of log()/ln() requires sqlite >=3.35 */
 
-SELECT LN(value/size + 1) AS norm_val
+SELECT LOG(value/size + 1) AS norm_val
     FROM assay
     JOIN (SELECT sampleID, SUM(value) AS size
             FROM assay
